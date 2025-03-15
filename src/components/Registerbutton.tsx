4f1ab3bf12c2c906ce900bfe68980a7523de22a8
lupescu141 <d.lupescu@hotmail.fi>
2025-03-15 14:37:36 +0200

    homepage css updated and added div flex containers for register and loggin buttons and text. Changed input buttons for normal buttons for better semantics.

--- src/components/Registerbutton.tsx
+++ src/components/Registerbutton.tsx
@@ -1,15 +1,34 @@
 import img from '../../public/button1.png';
+import {useState} from 'react';
 
 const RegisterButton = (/*{value, onClick} */) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const pointer = isHovered ? 'pointer' : 'auto';
+
   return (
-    <input
-      /*value={value}
-      onClick={onClick}*/
-      type="image"
-      alt="register"
-      src={img}
-      className="registerButton"
-    />
+    <button
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        background: 'none',
+        border: 'none',
+        cursor: `${pointer}`,
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      /*onClick={console.log('click')}*/
+    >
+      <img src={img} className="registerButton" alt="folder" />
+    </button>
   );
 };
 
